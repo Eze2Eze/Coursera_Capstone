833de537214dcc321916705d9901b4eaecb1d9ca
Eze2Eze <61146315+Eze2Eze@users.noreply.github.com>
2020-02-17 14:51:38 +0100

    Add files via upload

--- IBMDs_Capstone.py
+++ IBMDs_Capstone.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # This note book is going to be used for the IBM Data science Capstone project on Coursera
+
 # In[1]:
 
-
-# This note book is going to be used for the IBM Data science Capstone project on Coursera
 
 import pandas as pd
 import numpy as np
 
 print('Hello Capstone Project Course!')
-
-
-# In[ ]:
-
-
-
 
